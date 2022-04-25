e5453b2cf869004eee404d1ecd05eea990dd0347
Tung Dang <tungdt163@gmail.com>
2022-04-25 11:45:58 +0700

    image size update

--- Plugin/AddImageToGallery.php
+++ Plugin/AddImageToGallery.php
@@ -2,27 +2,59 @@
 
 namespace Scaleflex\Filerobot\Plugin;
 
+use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Catalog\Block\Product\View\Gallery;
+use Scaleflex\Filerobot\Helper\Filerobot;
 use Scaleflex\Filerobot\Model\FilerobotConfig;
+use Magento\Catalog\Helper\Image;
 
 class AddImageToGallery
 {
+    /** @var FilerobotConfig  */
     protected $fileRobotConfig;
 
+    /** @var ProductRepositoryInterface  */
+    protected $productRepository;
+
+    /** @var Image  */
+    protected $imageHelper;
+
+    /** @var Filerobot  */
+    protected $filerobotHelper;
+
+    /**
+     * @param FilerobotConfig $fileRobotConfig
+     */
     public function __construct(
-        FilerobotConfig $fileRobotConfig
+        FilerobotConfig $fileRobotConfig,
+        ProductRepositoryInterface $productRepository,
+        Image $imageHelper,
+        Filerobot $filerobotHelper
     ) {
         $this->fileRobotConfig = $fileRobotConfig;
+        $this->productRepository = $productRepository;
+        $this->imageHelper = $imageHelper;
+        $this->filerobotHelper = $filerobotHelper;
     }
 
     public function afterGetGalleryImages(Gallery $gallery, $images)
     {
         foreach ($images as $image) {
             if ($this->fileRobotConfig->isFilerobot($image->getData('file'))) {
-                $image->setData('url', $image->getData('file'));
-                $image->setData('small_image_url', $image->getData('file'));
-                $image->setData('medium_image_url', $image->getData('file'));
-                $image->setData('large_image_url', $image->getData('file'));
+                $url = $image->getData('file');
+                $entityId = $image->getData('entity_id');
+                $product  = $this->productRepository->getById($entityId);
+                if ($product) {
+                    $thumbImageSize = $this->imageHelper->init($product, 'product_thumbnail_image');
+                    $baseImageSize = $this->imageHelper->init($product, 'product_base_image');
+                    $url = $this->filerobotHelper->buildImageBySize($image->getData('file'), $baseImageSize->getWidth(), $baseImageSize->getHeight());
+                    $thumbnailUrl = $this->filerobotHelper->buildImageBySize($image->getData('file'), $thumbImageSize->getWidth(), $thumbImageSize->getHeight());
+                }
+
+                $image->setData('url', $url);
+                $image->setData('small_image_url', $thumbnailUrl);
+                $image->setData('medium_image_url',$url);
+                $image->setData('large_image_url', $url);
             }
         }
         return $images;
--- Plugin/Checkout/Image.php
+++ Plugin/Checkout/Image.php
@@ -3,7 +3,9 @@
 namespace Scaleflex\Filerobot\Plugin\Checkout;
 
 use Magento\Catalog\Api\ProductRepositoryInterface;
+use Scaleflex\Filerobot\Helper\Filerobot;
 use Scaleflex\Filerobot\Model\FilerobotConfig;
+use Magento\Catalog\Helper\Image as ImageHelper;
 
 class Image
 {
@@ -13,16 +15,29 @@ class Image
     /** @var FilerobotConfig  */
     protected FilerobotConfig $fileRobotConfig;
 
+    /** @var \Magento\Catalog\Helper\Image */
+    protected $imageHelper;
+
+    /** @var Filerobot  */
+    protected $filerobotHelper;
+
     /**
      * @param ProductRepositoryInterface $productRepository
      * @param FilerobotConfig $fileRobotConfig
+     * @param ImageHelper $imageHelper
+     * @param Filerobot $filerobotHelper
      */
     public function __construct(
         ProductRepositoryInterface $productRepository,
-        FilerobotConfig $fileRobotConfig
+        FilerobotConfig $fileRobotConfig,
+        ImageHelper $imageHelper,
+        Filerobot $filerobotHelper
+
     ) {
         $this->fileRobotConfig = $fileRobotConfig;
         $this->productRepository = $productRepository;
+        $this->imageHelper = $imageHelper;
+        $this->filerobotHelper = $filerobotHelper;
     }
 
     /**
@@ -36,9 +51,12 @@ class Image
         if ($result->getData('product_id')) {
             $productId = $result->getData('product_id');
             $product = $this->productRepository->getById($productId);
+            $thumbImageSize = $this->imageHelper->init($product, 'product_thumbnail_image');
             $images = $product->getMediaAttributeValues();
             if (!empty($images) && $images['thumbnail'] && $this->fileRobotConfig->isFilerobot($images['thumbnail'])) {
-                $result->setImageUrl($images['thumbnail']);
+                $result->setImageUrl($this->filerobotHelper->buildImageBySize($images['thumbnail'],
+                                                            $thumbImageSize->getWidth(),
+                                                            $thumbImageSize->getHeight()));
             }
         }
         return $result;
